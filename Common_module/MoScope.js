12f78bd35de53bf92305a5e53f732d3878cbf189
Toewailyun <toewailyun6@email.com>
2025-11-20 14:10:13 +0630

    Common_module

---
+++ Common_module/MoScope.js
@@ -0,0 +1,12 @@
+
+
+(function ( message ){
+    const superMan = "Superman";
+    const batMan = "Batman";
+    const spiderMan = "Spiderman";
+    console.log(`I am ${superMan}, ${batMan}, and ${spiderMan} - all in one! ${message}`);
+})( 'This is an IIFE example.' );
+
+//Module Wrapper
+
+// (function (exports, require, module, __filename, __dirname) { ... });
---
+++ Common_module/math.js
@@ -0,0 +1,6 @@
+//Improt Export Patterns
+
+module.exports.add = (a, b) => a + b;
+module.exports.subtract = (a, b) => a - b;
+
+//module.exports = { add, subtract };
