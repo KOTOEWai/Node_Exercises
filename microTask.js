5d3aa53b1f7b945f626c2f5aba5aba19e0f092cc
KOTOEWai <149601802+KOTOEWai@users.noreply.github.com>
2025-12-04 14:29:10 +0630

    addNpm

--- microTask.js
+++ microTask.js
@@ -1,15 +1,11 @@
-// index.js
 const fs = require("fs");
 
 fs.readFile(__filename, () => {
-  
-  setTimeout(() => console.log("this is setTimeout 1"), 1000);
-setImmediate(() => console.log("this is setImmediate 1"));
   console.log("this is readFile 1");
 });
 
 process.nextTick(() => console.log("this is process.nextTick 1"));
 Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+setTimeout(() => console.log("this is setTimeout 1"), 0);
+setImmediate(() => console.log("this is setImmediate"));
 
-
-for (let i = 0; i < 2000000000; i++) {}
